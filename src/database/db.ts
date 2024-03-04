675c67df30bb8c6cb13aa21f93707d039bd5a37b
Aayush Singh <97450185+aayushsingh7@users.noreply.github.com>
2024-03-04 14:00:46 +0530

    Update db.ts

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,19 +1,13 @@
-import { MongoClient, MongoClientOptions } from 'mongodb';
+import { MongoClient } from 'mongodb'
 
-const URI = process.env.DB_URL;
-const options: MongoClientOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-};
+const URI = process.env.DB_URL
+const options = {}
 
-if (!URI) {
-    throw new Error("[please add your mongodb URI to .env]");
-}
+if(!URI) throw new Error("[please add your mongodb URI to .env]")
 
-let client = new MongoClient(URI, options);
-let clientPromise = client.connect()
-    .catch((error) => {
-        throw new Error(`Error connecting to MongoDB: ${error}`);
-    });
+let client = new MongoClient(URI, options)
+let clientPromise = client.connect().catch((error) => { 
+    throw new Error(`Error connecting to MongoDB: ${error}`) 
+})
 
-export default clientPromise;
+export default clientPromise
